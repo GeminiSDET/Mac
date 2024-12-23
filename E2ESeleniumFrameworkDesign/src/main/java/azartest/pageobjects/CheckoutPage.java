8de6cfd403169abeeb3a2277b15244ecf849d1c4
Azar <azarriju@gmail.com>
2024-12-23 23:32:13 +0530

    Add files via upload

---
+++ E2ESeleniumFrameworkDesign/src/main/java/azartest/pageobjects/CheckoutPage.java
@@ -0,0 +1,96 @@
+package azartest.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import azartest.AbstractComponents.AbstractComponent;
+
+public class CheckoutPage extends AbstractComponent {
+
+	WebDriver driver;
+
+	public CheckoutPage(WebDriver driver) {
+		super(driver);
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+
+	}
+
+	@FindBy(css = ".action__submit")
+	 private WebElement submit;
+
+	@FindBy(css = "[placeholder='Select Country']")
+	private WebElement country;
+
+	@FindBy(xpath = "(//button[contains(@class,'ta-item')])[2]")
+	private WebElement selectCountry;
+
+	private By results = By.cssSelector(".ta-results");
+
+	public void selectCountry(String countryName) {
+		Actions a = new Actions(driver);
+		a.sendKeys(country, countryName).build().perform();
+		waitForElementToAppear(By.cssSelector(".ta-results"));
+		selectCountry.click();
+	}
+	
+	public ConfirmationPage submitOrder()
+	{
+		submit.click();
+		return new ConfirmationPage(driver);
+		
+		
+	}
+
+}
+
+
+
+/*
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import azartest.AbstractComponents.AbstractComponent;
+
+public class CheckoutPage extends AbstractComponent{
+
+	WebDriver driver;
+	public CheckoutPage(WebDriver driver)
+	{
+		super(driver);
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(css=".form-group")
+	WebElement country;
+	@FindBy(css=".ta-item:nth-of-type(2)")
+	WebElement selectCountrybutton;
+	@FindBy (css=".totalRow button")
+	WebElement submit;
+	
+	
+	By results =By.cssSelector(".ta-results");
+	
+	public void selectCountry(String countryName)
+	{
+		Actions a = new Actions(driver);
+		a.sendKeys(country,countryName).build().perform();
+		waitForElementToAppear(By.cssSelector(".ta-results"));
+		selectCountry.click();
+	}
+	
+	public ConfirmationPage submitOrder()
+	{
+		submit.click();
+		return new ConfirmationPage(driver);
+	}
+		
+}
+*/
